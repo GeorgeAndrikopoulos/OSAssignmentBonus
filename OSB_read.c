4b566582662fc5ba13fbca19261380f2544b4664
George <George@192.168.56.1>
2023-11-27 01:06:40 +0200

    commit 2

--- OSB_read.c
+++ OSB_read.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
+#include <unistd.h>
 
 int main() {
-  printf("Hello World!");
-  return 0;
+	FILE *fptr;
+
+	// Open a file in read mode
+	fptr = fopen("test_text_2000b.txt", "r");
+
+	if(fptr == NULL) {
+	  printf("Not able to open the file.");
+	}
+
+	// Store the content of the file
+	char myString[2000];
+
+	// Read the content and store it inside myString
+	fgets(myString, 1000, fptr);
+
+	// Print the file content
+	printf("%s", myString);
+
+	// Close the file
+	fclose(fptr);
+
+	return 0;
 }
