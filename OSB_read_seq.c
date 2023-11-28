4c59c68398c54e3819f8ea0f2c9ce887e7b750c0
GeorgeAndrikopoulos <geoandriofficial@gmail.com>
2023-11-28 17:32:37 +0200

    linux 2

--- OSB_read_seq.c
+++ OSB_read_seq.c
@@ -4,28 +4,36 @@
 #include <fcntl.h>
 
 int main() {
+	/*
 	// Open a file in read mode
-	//FILE *fptr;
-	//fptr = fopen("test_text_2000b.txt", "r");
-	//if(fptr == NULL) {
-	//	  printf("Not able to open the file.");
-	//	}
-	// Store the content of the file
-	//	char myString[2000];
+	FILE *fptr;
+	fptr = fopen("test_text_2000b.txt", "r");
+	if(fptr == NULL) {
+		  printf("Not able to open the file.");
+	}
+	//Store the content of the file
+	char myString[2000];
+
+	// Read the content and store it inside myString
+	fgets(myString, 2000, fptr);
+
+	// Print the file content
+	printf("%s\n", myString);
+
+	// Close the file
+	fclose(fptr);
+	*/
+
 	int fd;
 	char buff[1024];
 	char path[] = "/home/georgeand/git/OSAssignmentBonus/test_text_2000b.txt";
 
 	fd = open(path, O_RDONLY);
-	read(fd, buff, 500);
+	read(fd, buff, 1000);
 
 	printf("\n\n%s\n\n",buff);
 
-	// Print the file content
-	//printf("%s", myString);
 
-	// Close the file
-	//fclose(fptr);
 
 	return 0;
 }
